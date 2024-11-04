c359344f5ac5c7208439ae83c4763c5ea8a3002d
1ccw <140364678+1ccw@users.noreply.github.com>
2024-11-04 22:47:16 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 // 필요한 패키지를 불러옵니다.
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 const cors = require('cors');
 const path = require('path');
 
@@ -28,6 +29,23 @@ const htmlContent = `
 app.use(cors());
 app.use(bodyParser.json());
 
+// Mysql 데이터베이스 연결
+const db = mysql.createConnection({[
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'fingerprintDB',
+]});
+
+// 데이터베이스 연결
+db.connect((err) => {
+    if (err) {
+        console.error('데이터베이스 연결 실패:', err);
+        return;
+    }
+    console.log('데이터베이스에 연결되었습니다');
+});
+
 //정적 파일 제공 설정
 app.use(express.static('public'));
 
@@ -42,9 +60,14 @@ app.post('/api/fingerprint', (req, res) => {
     console.log('Received data:', { visitorId, deviceData });
     
     // 여기서 데이터를 데이터베이스에 저장하는 등의 처리를 할 수 있습니다.
-
+    const query = 'INSERT INTO fingerprints (visitor_id, device_data) VALUES (?, ?)';
+    db.query(query, [visitorId, JSON.stringify(deviceData)], (err, results) => {
+        if (err) {
+            console.error('데이터베이스 연결 실패: ' + err.stack);
+            return res.status(500).send({ status: 'error', message: '데이터베이스 연결 실패' });
+        }
     // 클라이언트에 성공 응답을 보냅니다.
-    res.send({ status: 'success' });
+    res.send({ status: 'success', id: results.insertID });
 });
 
 // 서버를 실행할 포트를 정의합니다.
