1e3060c8cd2b945691c0bdf9d6a62e7bb3ba4a23
1ccw <140364678+1ccw@users.noreply.github.com>
2024-11-07 12:23:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ FingerprintJS.load().then(fp => {
         console.log('Device Data:', deviceData);
         // 데이터 서버에 전송 또는 처리
         // 서버에 데이터 전송 예시
-        fetch('https://f895-175-125-49-71.ngrok-free.app/api/fingerprint', {
+        fetch('https://fc69-114-70-38-167.ngrok-free.app', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
